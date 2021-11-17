cef431bc6a50ce2a93dfe207bcc4dd2c6ac8f2fb
Nathan Daniels <nathandaniels@Nathans-MBP.attlocal.net>
2021-11-17 11:13:46 -0600

    Added starter questions to questions.js

--- questions.js
+++ questions.js
@@ -1,1 +1,33 @@
 //Your quiz questions array of objects goes here
+const questions = [
+    {
+        question: 'What country has the largest population in the world?',
+        category: 'geography',
+        correctAnswer: 'China',
+        incorrectAnswers: ['India', 'USA', 'Russia']
+    },
+    {
+        question: 'What city is home to the Golden Gate Bridge?',
+        category: 'geography',
+        correctAnswer: 'San Francisco',
+        incorrectAnswers: ['Los Angeles', 'Salt Lake City', 'Denver']
+    },
+    {
+        question: 'What is the largest country in the world?',
+        category: 'geography',
+        correctAnswer: 'Russia',
+        incorrectAnswers: ['Canada', 'USA', 'Norway']
+    },
+    {
+        question: 'What is the only US state that is home to no documented poisonous snakes?',
+        category: 'geography',
+        correctAnswer: 'Alaska',
+        incorrectAnswers: ['Montana', 'Wyoming', 'Maine']
+    },
+    {
+        question: 'What is the smallest country in the world?',
+        category: 'geography',
+        correctAnswer: 'Vatican City',
+        incorrectAnswers: ['Monaco', 'Liechtenstein', 'Maldives']
+    },
+]
